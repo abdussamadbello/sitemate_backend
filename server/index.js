5eb0c1ef1ea9c2f8fc364609763ee7447195cfb6
Abdussamad Bello <abdussamadbello4real@hotmail.co.uk>
2024-09-02 07:45:18 +0100

    server base creation

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const issueRoutes = require('./routes/issueRoutes');
+
+app.use(express.json());
+app.use('/issues', issueRoutes);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
